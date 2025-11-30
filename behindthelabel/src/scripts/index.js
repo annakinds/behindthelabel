408a44819e8c330cb350f16f936e55ffa0cedf4d
Anna Kinds <anna.kinds@student.howest.be>
2025-11-30 12:17:20 +0100

    subtitle scrollanimation works on detail pages

--- behindthelabel/src/scripts/index.js
+++ behindthelabel/src/scripts/index.js
@@ -3,11 +3,22 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.to(".subtitle", {
-    scrollTrigger: '.subtitle', 
-    x: 200,
-    duration: 1,
+const subtitles = document.querySelectorAll(".subtitle");
+subtitles.forEach((subtitle) => {
+    gsap.fromTo(
+        subtitle,
+        { autoAlpha: 0, x: -50 },
+        {
+            autoAlpha: 1,
+            x: 0,
+            duration: 0.6, 
+            scrollTrigger: {
+                trigger: subtitle,
+                start: "top 80%",
+                end: "bottom 20%",
+                toggleActions: "play reverse play reverse",
+                markers: false, 
+            },
+        }
+    );
 });
-
-
-
