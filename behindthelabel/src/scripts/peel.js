8e4f9e0a7b5b6265c0490e7f4c2fe8faf9ca8cf3
Anna Kinds <anna.kinds@student.howest.be>
2025-12-02 11:57:49 +0100

    horizontale peel off

--- behindthelabel/src/scripts/peel.js
+++ behindthelabel/src/scripts/peel.js
@@ -22,21 +22,44 @@ gsap.registerPlugin(ScrollTrigger);
 //     peelTL.reversed() ? peelTL.play() : peelTL.reverse();
 // }
 
-const peelTL = gsap.timeline({ paused: true, reversed: true, defaults: { duration: 0.75, ease: "power1.inOut" } })
+// const peelTL = gsap.timeline({ paused: true, reversed: true, defaults: { duration: 0.75, ease: "power1.inOut" } })
 
-peelTL
-    .to('#detail .sticky', { rotation: 10 }, 0)
-    .to('#detail .back', { height: 90, top: 110 }, 0)
-    .to('#detail .back .section', { marginTop: -50 }, 0)
-    .to('#detail .front', { height: 70, boxShadow: "0px -60px 10px -60px rgba(0,0,0,0.1)" }, 0)
-    .to('#detail .front .section', { marginTop: -90, backgroundColor: "#FF6249", backgroundPosition: "0 100px" }, 0)
-    .to('#detail h2', { duration: 0.05, opacity: 0 }, 0.3);
+// peelTL
+//     .to('#detail .sticky', { rotation: 10 }, 0)
+//     .to('#detail .back', { height: 90, top: 110 }, 0)
+//     .to('#detail .back .section', { marginTop: -50 }, 0)
+//     .to('#detail .front', { height: 70, boxShadow: "0px -60px 10px -60px rgba(0,0,0,0.1)" }, 0)
+//     .to('#detail .front .section', { marginTop: -90, backgroundColor: "#FF6249", backgroundPosition: "0 100px" }, 0)
+//     .to('#detail h2', { duration: 0.05, opacity: 0 }, 0.3);
 
-const sticker = document.querySelector('#detail')
+// const sticker = document.querySelector('#detail')
 
-sticker.addEventListener("mouseenter", peelDown);
-sticker.addEventListener("mouseleave", peelDown);
+// sticker.addEventListener("mouseenter", peelDown);
+// sticker.addEventListener("mouseleave", peelDown);
 
-function peelDown() {
-    peelTL.reversed() ? peelTL.play() : peelTL.reverse();
-}
+// function peelDown() {
+//     peelTL.reversed() ? peelTL.play() : peelTL.reverse();
+// }
+
+
+
+const fold = document.querySelector(".peel-fold");
+const rect = document.querySelector(".peel-rect");
+
+rect.addEventListener("mouseenter", () => {
+  gsap.to(fold, {
+    rotateX: 0,
+    rotateY: 0,
+    duration: 0.7,
+    ease: "power3.out"
+  });
+});
+
+rect.addEventListener("mouseleave", () => {
+  gsap.to(fold, {
+    rotateX: 90,
+    rotateY: 90,
+    duration: 0.7,
+    ease: "power3.in"
+  });
+});
