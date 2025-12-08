1d2ce12130a6b05c7a7faeb223cd95f2cb50fb27
Anna Kinds <anna.kinds@student.howest.be>
2025-12-08 15:33:51 +0100

    first chapter animation first detail page done

--- behindthelabel/src/scripts/detail.js
+++ behindthelabel/src/scripts/detail.js
@@ -30,22 +30,25 @@ const pinAndAnimate = ({
 
 export const setupScrollAnimations = () => {
     pinAndAnimate({
-        trigger: ".image-wrapper",
-        end: "+=100",
+        trigger: ".story",
         animations: [
-            { target: ".top1", vars: { x: "0%", opacity: 1 } },
-            { target: ".bottom1", vars: { x: "-0%", opacity: 1 } },
-            { target: ".mouth", vars: { x: "0%", opacity: 1 } },
-            { target: ".ear", vars: { x: "-0%", opacity: 1 } },
-            { target: ".bla1", vars: { x: "15vw", opacity: 1 } },
-            { target: ".bla2", vars: { x: "20vw", opacity: 1 } },
-            { target: ".bla3", vars: { x: "25vw", opacity: 1 } },
-            { target: ".aha1", vars: { x: "20vw", opacity: 1 } },
-            { target: ".aha2", vars: { x: "55vw", opacity: 1 } },
-            { target: ".aha3", vars: { x: "75vw", opacity: 1 } },
+            { target: ".top1", vars: { x: "0%", opacity: 1 }, position: 0 },
+            { target: ".bottom1", vars: { x: "-0%", opacity: 1 }, position: 0.1 },
+            { target: ".mouth", vars: { x: "0%", opacity: 1 }, position: 0.2 },
+            { target: ".ear", vars: { x: "-0%", opacity: 1 }, position: 0.3 },
+            { target: ".bla1", vars: { x: "15vw", opacity: 1 }, position: 0.4 },
+            { target: ".bla2", vars: { x: "20vw", opacity: 1 }, position: 0.5 },
+            { target: ".bla3", vars: { x: "25vw", opacity: 1 }, position: 0.6 },
+            { target: ".aha1", vars: { x: "20vw", opacity: 1 }, position: 0.7 },
+            { target: ".aha2", vars: { x: "55vw", opacity: 1 }, position: 0.8 },
+            { target: ".aha3", vars: { x: "75vw", opacity: 1 }, position: 0.9 },
+
+            { target: ".story__text1", vars: { opacity: 1 }, position: 1.2 },
         ],
         pin: ".story",
     });
+
+    
 
 };
 
