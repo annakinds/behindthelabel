8b9ae02e2838de9f5eece0b6021613d7aea5603e
Anna Kinds <anna.kinds@student.howest.be>
2025-12-11 11:55:22 +0100

    fix unused astro file

---
+++ behindthelabel/src/scripts/artworks/squarecircle.js
@@ -0,0 +1,24 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrambleTextPlugin } from "gsap/ScrambleTextPlugin";
+gsap.registerPlugin(ScrollTrigger, ScrambleTextPlugin);
+import { pinAndAnimate } from '../../components/Animate.js'
+
+export const setupScrollAnimations = () => {
+    const text1 = document.querySelector(".story__text1")?.textContent || "";
+    const text2 = document.querySelector(".story__text2")?.textContent || "";
+    const text3 = document.querySelector(".story__text3")?.textContent || "";
+    pinAndAnimate({
+        trigger: ".story",
+        end: "+=20000",
+        animations: [
+            //chapter 1
+            { target: ".top1", vars: { x: "0%", opacity: 1 }, position: 0 },
+            { target: ".bottom1", vars: { x: "-0%", opacity: 1 }, position: 0.1 },
+            
+        ],
+        pin: ".story",
+    });
+};
+
+
