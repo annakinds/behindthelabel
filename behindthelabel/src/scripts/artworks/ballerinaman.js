51e7b11ec50aae45f88154b27530cbe2924c1376
Anna Kinds <anna.kinds@student.howest.be>
2025-12-14 17:54:53 +0100

    ballerina and man detail page mobile done

--- behindthelabel/src/scripts/artworks/ballerinaman.js
+++ behindthelabel/src/scripts/artworks/ballerinaman.js
@@ -15,66 +15,96 @@ export const setupScrollAnimations = () => {
             //chapter 1
             { target: ".top1", vars: { x: "0%", opacity: 1 }, position: 0 },
             { target: ".bottom1", vars: { x: "-0%", opacity: 1 }, position: 0.1 },
-
-
-            // {
-            //     target: ".story__text1",
-            //     vars: { opacity: 1 },
-            //     position: 1.8,
-            //     scramble: {
-            //         text: text1,
-            //         chars: ".",
-            //         speed: 0.3,
-            //         revealDelay: 0.05,
-            //     }
-            // },
-
-
-            // { target: ".story__text1", vars: { opacity: 0 }, position: 2.7 },
-            // { target: ".top1", vars: { x: "-100%", opacity: 1 }, position: 3 },
-            // { target: ".bottom1", vars: { x: "100%", opacity: 1 }, position: 3 },
+            { target: ".binoculars", vars: { opacity: 1 }, position: 0.2 },
+            { target: ".watching1", vars: { opacity: 1 }, position: 0.3 },
+            { target: ".binoculars", vars: { rotate:"50deg" }, position: 0.6 },
+            { target: ".watching2", vars: { opacity: 1 }, position: 0.6 },
+            { target: ".binoculars", vars: { rotate: "0deg" }, position: 0.9 },
+            { target: ".watching3", vars: { opacity: 1 }, position: 0.9 },
+            {
+                target: ".story__text1",
+                vars: { opacity: 1 },
+                position: 1,
+                scramble: {
+                    text: text1,
+                    chars: ".",
+                    speed: 0.3,
+                    revealDelay: 0.05,
+                }
+            },
+            { target: ".binoculars", vars: { opacity:0 }, position: 1.5 },
+            { target: ".watching1", vars: { opacity: 0 }, position: 1.5 },
+            { target: ".watching2", vars: { opacity: 0 }, position: 1.5 },
+            { target: ".watching3", vars: { opacity: 0 }, position: 1.5 },
+            { target: ".story__text1", vars: { opacity: 0 }, position: 1.6 },
+            { target: ".top1", vars: { x: "-100%", opacity: 1 }, position: 1.7 },
+            { target: ".bottom1", vars: { x: "100%", opacity: 1 }, position: 1.7 },
 
             // //chapter 2
-            // { target: ".top2", vars: { x: "0%", opacity: 1 }, position: 3.1 },
-
-            // {
-            //     target: ".story__text2",
-            //     vars: { opacity: 1 },
-            //     position: 5,
-            //     scramble: {
-            //         text: text2,
-            //         chars: ".",
-            //         speed: 0.3,
-            //         revealDelay: 0.05,
-            //     }
-            // },
-          
-            // { target: ".story__text2", vars: { opacity: 0 }, position: 5.6 },
-            // { target: ".top2", vars: { x: "-100%", opacity: 1 }, position: 5.6 },
+            { target: ".top2", vars: { x: "0%", opacity: 1 }, position: 1.8 },
+            { target: ".bottom2", vars: { x: "-0%", opacity: 1 }, position: 1.8 },
+            { target: ".yellowstar", vars: { opacity: 1 }, position: 1.9 },
+            { target: ".me", vars: { opacity: 1 }, position: 1.9 },
+            { target: ".cloud-top", vars: { left:"0vw" }, position: 2.2 },
+            { target: ".cloud-bottom", vars: { right: "0vw" }, position: 2.2 },
+            {
+                target: ".story__text2",
+                vars: { opacity: 1 },
+                position: 2.3,
+                scramble: {
+                    text: text2,
+                    chars: ".",
+                    speed: 0.3,
+                    revealDelay: 0.05,
+                }
+            },
+            { target: ".yellowstar", vars: { opacity: 0 }, position: 3 },
+            { target: ".me", vars: { opacity: 0 }, position: 3 },
+            { target: ".cloud-top", vars: { opacity:0}, position: 3 },
+            { target: ".cloud-bottom", vars: { opacity:0}, position: 3 },
+            { target: ".story__text2", vars: { opacity: 0 }, position: 3.1 },
+            { target: ".top2", vars: { x: "-100%", opacity: 1 }, position: 3.2 },
+            { target: ".bottom2", vars: { x: "100%", opacity: 1 }, position: 3.2 },
 
             // //chapter 3
-            // { target: ".top3", vars: { x: "0%", opacity: 1 }, position: 5.7 },
-          
-
-            // {
-            //     target: ".story__text3",
-            //     vars: { opacity: 1 },
-            //     position: 6.5,
-            //     scramble: {
-            //         text: text3,
-            //         chars: ".",
-            //         speed: 0.3,
-            //         revealDelay: 0.05,
-            //     }
-            // },
-           
-            // { target: ".story__text3", vars: { opacity: 0 }, position: 7.5 },
-            // { target: ".top3", vars: { x: "-100%", opacity: 1 }, position: 7.6 },
+            { target: ".top3", vars: { x: "0%", opacity: 1 }, position: 3.3 },
+            { target: ".bottom3", vars: { x: "-0%", opacity: 1 }, position: 3.3 },
+            { target: ".hammer1", vars: { opacity: 1 }, position: 3.4 },
+            { target: ".hammer2", vars: { opacity: 1 }, position: 3.4 },
+            { target: ".chisel1", vars: { opacity: 1 }, position: 3.4 },
+            { target: ".chisel2", vars: { opacity: 1 }, position: 3.4 },
+            { target: ".hammer1", vars: { rotate: "-40deg" }, position: 3.5 },
+            { target: ".hammer2", vars: { rotate: "140deg" }, position: 3.5 },
+            { target: ".bam1", vars: { opacity: 1 }, position: 3.6 },
+            { target: ".bam2", vars: { opacity: 1 }, position: 3.6 },
+            { target: ".bigstar1", vars: { opacity: 1 }, position: 3.6 },
+            { target: ".bigstar2", vars: { opacity: 1 }, position: 3.6 },
+            {
+                target: ".story__text3",
+                vars: { opacity: 1 },
+                position: 3.7,
+                scramble: {
+                    text: text3,
+                    chars: ".",
+                    speed: 0.3,
+                    revealDelay: 0.05,
+                }
+            },
+            { target: ".chisel1", vars: { opacity: 0 }, position: 4.2 },
+            { target: ".chisel2", vars: { opacity: 0 }, position: 4.2 },
+            { target: ".hammer1", vars: { opacity:0 }, position: 4.2},
+            { target: ".hammer2", vars: { opacity:0 }, position: 4.2 },
+            { target: ".bam1", vars: { opacity: 0 }, position: 4.2 },
+            { target: ".bam2", vars: { opacity: 0 }, position: 4.2 },
+            { target: ".bigstar1", vars: { opacity: 0 }, position: 4.2 },
+            { target: ".bigstar2", vars: { opacity: 0 }, position: 4.2 },
+            { target: ".story__text3", vars: { opacity: 0 }, position: 4.3 },
+            { target: ".top3", vars: { x: "-100%", opacity: 1 }, position: 4.4 },
+            { target: ".bottom3", vars: { x: "100%", opacity: 1 }, position: 4.4 },
 
             // //map
-            // { target: ".top4", vars: { x: "0%", opacity: 1 }, position: 8 },
+            { target: ".top4", vars: { x: "0%", opacity: 1 }, position: 4.5 },
             // { target: ".map", vars: { x: "0%", opacity: 1 }, position: 10.3 },
-
         ],
         pin: ".story",
     });
