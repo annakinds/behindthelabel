f5630d8229fca7b3b254a6a800d4edae84a8fd3f
Ashley Vanneste <ashley.vanneste@student.howest.be>
2025-12-17 12:16:12 +0100

    links on detail pages

--- behindthelabel/src/components/Peel.js
+++ behindthelabel/src/components/Peel.js
@@ -2,19 +2,31 @@ import { gsap } from "gsap";
 
 export const startPeel = (onComplete) => {
     const peelContainer = document.querySelector(".peel-container");
-    const peelEffect = peelContainer.querySelector(".peel-effect");
+    const peelEffect = peelContainer.querySelector(".triangle-mask");
     const triangleFill = peelContainer.querySelector(".triangle-fill");
     const cornerPeel = peelContainer.querySelector(".corner-peel");
 
-    gsap.set('.start.content', {
-        clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)"
+    gsap.set(peelEffect, {
+        clipPath: "polygon(100% 100%, 100% 100%, 100% 100%, 100% 100%)"
     });
 
     const tl = gsap.timeline({
-        onComplete: () => {
-            if (onComplete) onComplete();
-        }
+        onComplete: () => onComplete && onComplete()
     });
+
+    tl.to(peelEffect, {
+        clipPath: "polygon(100% 100%, 0% 100%, 0% 0%, 100% 0%)",
+        duration: 1,
+        ease: "power2.out"
+    }, 0);
+
+    // Collapse mask to top left
+    tl.to(peelEffect, {
+        clipPath: "polygon(0% 0%, 0% 0%, 0% 0%, 0% 0%)",
+        duration: 1,
+        ease: "power2.inOut"
+    }, 1);
+
 
     tl.to(cornerPeel, {
         width: "105%",
@@ -23,17 +35,11 @@ export const startPeel = (onComplete) => {
         ease: "power2.out"
     }, 0);
 
-    tl.to('.start.content', {
-        clipPath: "polygon(0% 0%, 0% 0%, 0% 0%, 0% 0%)",
-        duration: 1,
-        ease: "power2.out"
-    }, 0);
-
-    tl.to(triangleFill, {
+   tl.to(triangleFill, {
         x: "-120%",
         y: "-260%",
         scale: 1.2,
-        duration: 1,
+         duration: 1,
         ease: "power2.in"
-    }, 0.6);
+     }, 0.6);
 };
