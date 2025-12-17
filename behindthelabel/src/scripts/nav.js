607eb465cf24779983173ddf4f5073d5b0b4de5f
Anna Kinds <anna.kinds@student.howest.be>
2025-12-17 18:15:02 +0100

    fix navigation buttons

--- behindthelabel/src/scripts/nav.js
+++ behindthelabel/src/scripts/nav.js
@@ -1,7 +1,16 @@
 const hamburger = document.querySelector(".hamburger");
+const navItem = document.querySelectorAll(".nav-item");
+
 const menu = document.querySelector(".nav-menu");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     menu.classList.toggle("active");
+});
+
+navItem.forEach((item) => {
+    item.addEventListener("click", () => {
+        hamburger.classList.toggle("active");
+        menu.classList.toggle("active");
+    });
 });
